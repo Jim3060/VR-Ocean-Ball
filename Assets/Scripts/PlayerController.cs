2e07c98bf7c4a877d2b6f7133c1b5aa1b9d1c50c
LAPTOP-GGBJ45QO\alw <liujunyi>
2018-02-25 14:04:40 +0800

    initial the project with roll a ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    public Text countText;
+    public Text winText;
+    public float speed;
+    public float rotationSpeed;
+    private int count;
+    public float horizonRotation;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    private void FixedUpdate()
+    {
+        DoRotationMovement();
+        DoJump();
+    }
+
+    private void DoRotationMovement() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        bool leftRotate = Input.GetKey(KeyCode.Q);
+        bool rightRotate = Input.GetKey(KeyCode.E);
+        float rotate = 0;
+        if (leftRotate) { rotate -= 1; }
+        if (rightRotate) { rotate += 1; }
+        horizonRotation = rotate;
+        Vector3 horizontalRotation = new Vector3(0, rotate, 0);
+
+
+        Vector3 angleV = new Vector3(0, 0, moveVertical);
+        Vector3 angleH = new Vector3(moveHorizontal, 0, 0);
+        Vector3 crossv = Vector3.Cross(Vector3.up, angleV);
+        Vector3 crossh = Vector3.Cross(Vector3.up, angleH);
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddTorque(crossv + crossh+horizontalRotation);
+
+    }
+
+    private void DoJump() {
+        bool moveJump = Input.GetKeyDown(KeyCode.Space);
+        if (moveJump) { Debug.Log("jump"); rb.AddForce(Vector3.up * 80 * speed); }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 7) {
+            winText.text = "YOU WIN";
+        }
+    }
+}
+
---
+++ Assets/Scripts/SlidingDoorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoorController : MonoBehaviour {
+    public PlayerController pc;
+    public GameObject mydoor;
+
+    private float translateSpeed = 0.01f;
+	// Use this for initialization
+	void Start () {
+        mydoor = gameObject.GetComponentInChildren<Rigidbody>().gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.gameObject.layer);
+        if (other.gameObject.layer == 10)//player layer
+        {
+            Debug.Log(other.gameObject);
+            float r = translateSpeed * pc.horizonRotation;
+            
+            BoxCollider box = gameObject.GetComponentInChildren<BoxCollider>();
+
+            if ((mydoor.transform.localPosition.x <= box.size.x&&r>0)||(mydoor.transform.localPosition.x>=0 &&r<0))
+            {
+                mydoor.transform.Translate(new Vector3(r, 0, 0));
+                box.center = new Vector3(mydoor.transform.localPosition.x, box.center.y, box.center.z);
+            }
+            
+        }
+       
+    }
+}
