6bb1ce402a4b0ad4febe1cad3242f60ab8f4a5c6
rebas777 <154587260@qq.com>
2018-02-26 16:30:31 +0800

    build environment of the first scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,95 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
-    public Text countText;
-    public Text winText;
+    
     public float speed;
     public float rotationSpeed;
-    private int count;
     public float horizonRotation;
+
+    public ControllerManager manager;
+
+    bool ground = true;
     private void Start()
     {
+        ground = true;
         rb = GetComponent<Rigidbody>();
-        count = 0;
-        SetCountText();
-        winText.text = "";
+        
     }
     private void FixedUpdate()
     {
+        DoMovementVR();
         DoRotationMovement();
         DoJump();
+    }
+
+    private List<Vector3> sampleLeftPoints=new List<Vector3>();
+    private List<Vector3> sampleRightPoints=new List<Vector3>();
+    public int sampleGap=1;
+    private int sampleCount=0;
+
+    private void updateSamplePoints() {
+        sampleCount += 1;
+        if (sampleCount < sampleGap) { return; }
+        else { sampleCount = 0; }
+        if (manager.leftControllerTouchDown.grip)
+        {
+            if (sampleLeftPoints.Count != 3)
+            {
+                sampleLeftPoints.Add(manager.leftControllerPosition);
+
+            }
+            else {
+                sampleLeftPoints[0] = sampleLeftPoints[1];
+                sampleLeftPoints[1] = sampleLeftPoints[2];
+                sampleLeftPoints[2] = manager.leftControllerPosition;
+            }
+           
+        }
+        else {
+            sampleLeftPoints.Clear();
+        }
+
+
+        if (manager.rightControllerTouchDown.grip)
+        {
+            if (sampleRightPoints.Count != 3)
+            {
+                sampleRightPoints.Add(manager.rightControllerPosition);
+
+            }
+            else
+            {
+                sampleRightPoints[0] = sampleRightPoints[1];
+                sampleRightPoints[1] = sampleRightPoints[2];
+                sampleRightPoints[2] = manager.rightControllerPosition;
+            }
+
+        }
+        else
+        {
+            sampleRightPoints.Clear();
+        }
+    }
+
+    private void DoMovementVR() {
+        updateSamplePoints();
+        Vector3 result = Vector3.zero;
+        Vector3 left,right;
+       
+        if (sampleLeftPoints.Count == 3) {
+            left = Vector3.Cross((sampleLeftPoints[0] - sampleLeftPoints[1]), (sampleLeftPoints[1] - sampleLeftPoints[2]));
+            result += left;
+            Debug.Log("left m:"+left);
+            Debug.Log(sampleLeftPoints);
+        }
+        if (sampleRightPoints.Count == 3)
+        {
+            right = Vector3.Cross((sampleRightPoints[0] - sampleRightPoints[1]), (sampleRightPoints[1] - sampleRightPoints[2]));
+            result += right;
+            Debug.Log("right m:" + right);
+            Debug.Log(sampleRightPoints);
+        }
+        rb.AddTorque(result);
     }
 
     private void DoRotationMovement() {
@@ -43,28 +115,29 @@ public class PlayerController : MonoBehaviour {
 
         //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddTorque(crossv + crossh+horizontalRotation);
+        //Debug.Log(rb.angularVelocity);
 
     }
 
     private void DoJump() {
         bool moveJump = Input.GetKeyDown(KeyCode.Space);
-        if (moveJump) { Debug.Log("jump"); rb.AddForce(Vector3.up * 80 * speed); }
+        if (ground == true && moveJump)
+        {
+            
+            
+            Debug.Log("jump"); rb.AddForce(Vector3.up * 80 * speed); 
+            ground = false;
+        }
+        
+        
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject.CompareTag("Pick Up")) {
-            other.gameObject.SetActive(false);
-            count++;
-            SetCountText();
-        }
+        ground = true;
     }
 
-    void SetCountText() {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 7) {
-            winText.text = "YOU WIN";
-        }
-    }
+ 
+   
 }
 
--- Assets/Scripts/SlidingDoorController.cs
+++ Assets/Scripts/SlidingDoorController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SlidingDoorController : MonoBehaviour {
-    public PlayerController pc;
+   // public PlayerController pc;
+    public Rigidbody playerBody;
     public GameObject mydoor;
 
-    private float translateSpeed = 0.01f;
+    private float translateSpeed = 0.005f;
 	// Use this for initialization
 	void Start () {
         mydoor = gameObject.GetComponentInChildren<Rigidbody>().gameObject;
@@ -23,13 +24,15 @@ public class SlidingDoorController : MonoBehaviour {
         if (other.gameObject.layer == 10)//player layer
         {
             Debug.Log(other.gameObject);
-            float r = translateSpeed * pc.horizonRotation;
-            
+            //float r = translateSpeed * pc.horizonRotation;
+            Vector3 rv = Vector3.Cross((other.transform.position - transform.position), playerBody.angularVelocity);
+           // float r = translateSpeed * playerBody.angularVelocity.y;
+
             BoxCollider box = gameObject.GetComponentInChildren<BoxCollider>();
 
-            if ((mydoor.transform.localPosition.x <= box.size.x&&r>0)||(mydoor.transform.localPosition.x>=0 &&r<0))
+            if ((mydoor.transform.localPosition.x <= box.size.x&&rv.x>0)||(mydoor.transform.localPosition.x>=0 &&rv.x<0))
             {
-                mydoor.transform.Translate(new Vector3(r, 0, 0));
+                mydoor.transform.Translate(new Vector3(translateSpeed * rv.x, 0, 0));
                 box.center = new Vector3(mydoor.transform.localPosition.x, box.center.y, box.center.z);
             }
             
